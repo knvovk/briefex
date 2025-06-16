cf521c5d949c10693c83bef71876746cbf59f931
knvovk <knvovk@outlook.com>
2025-06-16 22:31:58 +0300

    Refactor fetcher imports for improved clarity and organization in `__init__.py`

--- src/crawlers/fetchers/__init__.py
+++ src/crawlers/fetchers/__init__.py
@@ -1,4 +1,4 @@
-from .base import BaseFetcher, BaseFetcherFactory
-from .factory import FetcherFactory
+from .base import BaseFetcher
+from .factory import BaseFetcherFactory, FetcherFactory
 from .html import HTMLFetcher
 from .rss import RSSFetcher
