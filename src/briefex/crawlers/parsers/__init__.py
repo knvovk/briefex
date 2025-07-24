f31e4d4c683e7062dfd753e93406ba49c6b916bf
knvovk <knvovk@outlook.com>
2025-07-24 22:19:36 +0300

    refactor(briefex): package `crawlers` was replaced with `crawler`.

--- src/briefex/crawlers/parsers/__init__.py
+++
@@ -1,7 +0,0 @@
-import briefex.crawlers.parsers.html  # noqa: F401
-import briefex.crawlers.parsers.rss  # noqa: F401
-
-from .base import Parser
-from .factory import ParserFactory, create_parser_factory
-
-__all__ = ["Parser", "ParserFactory", "create_parser_factory"]
