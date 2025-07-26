a067477e5a71c6931307d7fad39a6694bdfdf548
knvovk <knvovk@outlook.com>
2025-07-26 13:08:40 +0300

    refactor(storage)!: Refactor storage exports and add `get_default_*_factory`.

--- src/briefex/storage/__init__.py
+++ src/briefex/storage/__init__.py
@@ -1,16 +1,60 @@
-import briefex.storage.post  # noqa: F401
-import briefex.storage.source  # noqa: F401
+from __future__ import annotations
 
-from .base import Storage
-from .factory import StorageFactory, create_storage_factory
-from .models import Post, PostStatus, Source, SourceType
+from briefex.storage.base import (
+    PostStorage,
+    PostStorageFactory,
+    SourceStorage,
+    SourceStorageFactory,
+)
+from briefex.storage.exceptions import (
+    DuplicateObjectError,
+    ObjectNotFoundError,
+    StorageConfigurationError,
+    StorageConnectionError,
+    StorageException,
+)
+from briefex.storage.factory import (
+    DefaultPostStorageFactory,
+    DefaultSourceStorageFactory,
+)
+from briefex.storage.models import Post, PostStatus, Source, SourceType
+
+_source_storage_factory: SourceStorageFactory | None = None
+_post_storage_factory: PostStorageFactory | None = None
+
+
+def get_default_source_storage_factory() -> SourceStorageFactory:
+    global _source_storage_factory
+
+    if _source_storage_factory is None:
+        _source_storage_factory = DefaultSourceStorageFactory()
+
+    return _source_storage_factory
+
+
+def get_default_post_storage_factory() -> PostStorageFactory:
+    global _post_storage_factory
+
+    if _post_storage_factory is None:
+        _post_storage_factory = DefaultPostStorageFactory()
+
+    return _post_storage_factory
+
 
 __all__ = [
-    "Storage",
-    "StorageFactory",
-    "create_storage_factory",
+    "PostStorage",
+    "PostStorageFactory",
+    "SourceStorage",
+    "SourceStorageFactory",
+    "DuplicateObjectError",
+    "ObjectNotFoundError",
+    "StorageConfigurationError",
+    "StorageConnectionError",
+    "StorageException",
     "Post",
     "PostStatus",
     "Source",
     "SourceType",
+    "get_default_source_storage_factory",
+    "get_default_post_storage_factory",
 ]
